a2b400cd77e998f058681d1ffddcd5c1d29d16fe
Matsuya Naoki <naokingus627@gmail.com>
2021-09-19 12:16:26 +0900

    suppliment: データ作成方法を変更
    - train, test別々にしていたものを、結合して編集
    - 今後、ラグ特徴量など追加予定

--- src/Preprocessor.py
+++ src/Preprocessor.py
@@ -3,6 +3,8 @@ import numpy as np
 
 import os
 
+import global_valiables
+
 class Preprocessor:
     def __init__(self):
         pass
@@ -11,14 +13,18 @@ class Preprocessor:
         self._df_org_category_names = pd.read_csv(inp_dir+'category_names.csv')
         self._df_org_item_categories = pd.read_csv(inp_dir+'item_categories.csv')
         self._df_org_sales_history = pd.read_csv(inp_dir+'sales_history.csv', parse_dates=['日付'])
-        self._df_org_test = pd.read_csv(inp_dir+'test.csv')
+        self._df_org_test = pd.read_csv(inp_dir+'test.csv', index_col=0)
     
     def createData(self, beginDate, endDate, purposeDate):
         df_products = self._df_org_item_categories.copy()
         df_products = df_products.merge(self._df_org_category_names, on='商品カテゴリID')
 
+        ####################
+        # trainデータ編集
+
+        ## sales_historyから必要情報を抽出
         df_train = self._df_org_sales_history[['日付', '店舗ID', '商品ID', '売上個数']]
-        df_train = df_train.loc[beginDate:endDate]
+        ## 売上個数を日別データから月別データに変換
         df_train = df_train.set_index(['日付', '店舗ID', '商品ID'])
         df_train = df_train.groupby(
             [
@@ -28,23 +34,34 @@ class Preprocessor:
             ]
         ).sum()
         df_train = df_train.reset_index(['店舗ID', '商品ID'])
-        df_train['year'] = df_train.index.year
-        df_train['month'] = df_train.index.month
 
-        print('df_train:\n{}'.format(df_train))
+        ###################
+        # testデータ編集
 
+        ## test.csvをコピー
         df_test = self._df_org_test.copy()
-        df_test['year'] = pd.to_datetime(purposeDate).year
-        df_test['month'] = pd.to_datetime(purposeDate).month
-
+        ## 商品情報を追加
         df_test = df_test.merge(df_products, on='商品ID')
-
-        df_test.set_index('index', inplace=True)
-        df_test.drop('商品カテゴリ名', axis=1, inplace=True)
-
+        ## datetime index変更
+        df_test['日付'] = pd.to_datetime(purposeDate)
+        df_test.set_index('日付', inplace=True)
+        ## trainデータの列順に合わせる
         df_test = df_test.reindex(columns=df_train.columns)
-        df_test.drop('売上個数', axis=1, inplace=True)
 
-        print('df_test:\n{}'.format(df_test))
+        ###################
+        # 結合して編集
 
-        return df_train.drop('売上個数', axis=1), df_train['売上個数'], df_test
+        ## train, test結合
+        df_concat = pd.concat([df_train, df_test], axis=0)
+        ## year, month列追加
+        df_concat['year'] = df_concat.index.year
+        df_concat['month'] = df_concat.index.month
+
+        train = df_concat.loc[beginDate:endDate]
+        test = df_concat.loc[purposeDate]
+
+        if global_valiables.verbose:
+            print('trainData:\n{}'.format(train))
+            print('testData:\n{}'.format(test))
+
+        return train.drop('売上個数', axis=1), train['売上個数'], test.drop('売上個数', axis=1)
